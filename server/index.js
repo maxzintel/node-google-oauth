d65568f048ca9de2f68fd860176326f2f4df759b
s1dequest <maxzintel@gmail.com>
2019-11-01 17:00:57 -0400

    Add options to OAuth strategy. Create callback URL for passport. Will be the route the users take after they grant our app permission to authenticate via Google.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy()); 
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleSecret,
+    callbackURL: '/auth/google/callback' // The route the user will be sent to after they grant our application permission.
+    // TO DO: Route Handler for the above.
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
 // Creates a new instance of the passport google strategy.
 // Tells the app that we want to be able to authenticate users via Google.
 // Add options to configure how this strategy works in our environment. ClientID and ClientSecret. Given to us by Google's oauth service.
