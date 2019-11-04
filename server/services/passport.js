f757cb0b0048123fef8a995a9e8e1951ca677f63
s1dequest <maxzintel@gmail.com>
2019-11-04 15:51:31 -0500

    MongoDB: Edit callback function in passport to check if we already have a user with the current google profile id. Eliminates duplicates.

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
 
-const User = mongoose.model('users');
+const User = mongoose.model('users'); // Model Class = User
 
 passport.use(
   new GoogleStrategy({
@@ -11,6 +11,15 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback' // The route the user will be sent to after they grant our application permission.
   }, (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save(); // Creates a distinct model instance. ".save()" takes the model instance and saves it to the database.
+    User.findOne({ googleId: profile.id }) // Returns a promise
+      .then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile id.
+        } else {
+          // make a new record
+          new User({ googleId: profile.id }).save();
+          // Creates a distinct model instance. ".save()" takes the model instance and saves it to the database.
+        }
+      })
   })
 );
