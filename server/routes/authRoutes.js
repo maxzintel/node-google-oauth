63b08fc15e717a427350311017ed83d1b3349ea5
s1dequest <maxzintel@gmail.com>
2019-11-07 16:50:09 -0500

    Authentication test. Response should contain the user for someone who has passed through the OAuth flow.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,6 +9,11 @@ module.exports = (app) => { // Export this function from this file.
       // "give us this users profile information and email as well."
     })
   );
-  
+
   app.get('/auth/google/callback', passport.authenticate('google')); // code in the URL is now available, user has given us permission.
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user) // Shows that someone who has gone through the oauth flow receives a response...
+    // ... aka has access to the user!
+  });
 };
