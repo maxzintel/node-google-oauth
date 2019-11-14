b6710726ba8b9211036db3c7330b3fb6132c90fd
s1dequest <maxzintel@gmail.com>
2019-11-14 14:41:29 -0500

    TravisCI: Create travis.yml and alter deploy.sh to function for our current needs.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,3 @@ docker build -t s1dequest/oauth-server:latest -t s1dequest/oauth-server:$SHA -f 
 
 docker push s1dequest/oauth-server:latest
 docker push s1dequest/oauth-server:$SHA
-
-kubectl apply -f k8s
-
-kubectl set image deployments/server-deployment server=s1dequest/oauth-server:$SHA
